09832f865519b1425a2fa3b7204b932e6830254a
Waad Alkatheri <walquhum@stu.kau.edu.sa>
2018-08-28 06:00:53 +0300

    Add files via upload

---
+++ AreaTriangleChallenge.playground/Contents.swift
@@ -0,0 +1,53 @@
+//  ClassObjectActivity
+//
+//  Created by mac on 8/14/18.
+//  Copyright © 2018 mac. All rights reserved.
+//
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+
+class AreaTriangle  {
+    
+    //TODO: Decleration width property as a optional
+    var base: Int = 5;
+    
+    //TODO: Decleration height property as a optional
+    var height: Int = 10;
+    
+    // Default Constructor (No parameter)
+    // (Used to create instance).
+    init()  {
+        
+    }
+    
+    //TODO : Contructor with 2 parameters.(Used to create instance)
+    // self.base refers to the base property of the class
+  
+    
+    //TODO:  A method calculates the area of the Triangle and return Area of Triangle  -> AreaTriangle = (self.base * self.height)/2
+    
+    
+}
+
+print("Create a Triangle ");
+
+//TODO:  Create Rectangle objec via default constructor: init()
+
+
+//TODO: Print out width, height
+
+// TODO: Call the method to calculate the area and print it.
+
+
+print("---------")
+
+print("Create a Triangle 2");
+
+//TODT: call Triangle object via contructor with 2 parameters: init(Int,Int)
+
+
+
+
+//TODO: Call the method to calculate the area and print it.
